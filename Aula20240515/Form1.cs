ee96ab355deb788f0dc2412ff6622422d8d955bd
Aura <111578898+analaurabrito@users.noreply.github.com>
2024-05-15 08:03:58 -0300

    Add files via upload

---
+++ Aula20240515/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aula20240515
+{
+    public partial class Form1 : Form
+    {
+        List<Item> lista;
+
+        public Form1()
+        {
+            InitializeComponent();
+            lista = new List<Item>();
+            comboBox1.DisplayMember = "nome";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Item aux = new Item(comboBox1.Items.Count + 1, 
+                ("OPÇÃO " + (comboBox1.Items.Count + 1)).ToString());
+            lista.Add(aux);
+            comboBox1.Items.Add(aux);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = comboBox1.Items;
+            dataGridView1.Refresh();
+
+            //dataGridView1.Columns["id"].Visible = false;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                Item i = dataGridView1.SelectedRows[0].DataBoundItem as Item;
+                MessageBox.Show("ITEM ("+i.id+" - "+i.nome+")");
+
+            }
+            catch {
+                MessageBox.Show("ERRO");
+            }
+        }
+    }
+}
---
+++ Aula20240515/Item.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula20240515
+{
+    internal class Item
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+
+        public Item(int id, string nome)
+        {
+            this.id = id;
+            this.nome = nome;
+        }
+    }
+}
